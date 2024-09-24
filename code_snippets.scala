1d5f2ccb815befe1fb77b9273ed3b654f188fdfe
Mattia <mattia.metzler@gmail.com>
2024-09-24 10:38:23 +0200

    update code_snippets.scala

--- code_snippets.scala
+++ code_snippets.scala
@@ -60,3 +60,74 @@ extension (r: Rational)
 extension (x: Rational)
 	def + (y: Rational): Rational = x.add(y)
 	def * (y: Rational): Rational = x.mul(y)
+
+// ==========================================================
+
+// IntSet class hierarchy from 3.1
+abstract class IntSet:
+	def contains(x: Int): Boolean
+	def including(x: Int): IntSet
+
+object Empty extends IntSet:
+	def contains(x: Int): Boolean = false
+	def including(x: Int): IntSet = NonEmpty(s, Empty(), Emtpy())
+end Empty
+
+class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet:
+	def contains(x: Int): Boolean =
+		if x < elem then left.contains(x)
+		else if x > elem then right.contains(x)
+		else true
+
+	def incl(x: Int): IntSet =
+		if x < elem then NonEmpty(elem, left.incl(x), right)
+		else if x > elem then NonEmpty(elem, left, right.incl(x))
+		else this
+end NonEmpty
+
+// ==========================================================
+
+// Expr class hierarchy from 3.2 and 3.3 - Solution 1
+trait Expr:
+	def eval: Int
+
+class Number(n: Int) extends Expr:
+	def eval: Int = n
+
+class Sum(e1: Expr, e2: Expr) extends Expr:
+	def eval: Int = e1.eval + e2.eval
+
+// Solution 2
+enum Expr:
+	case Var(s: String)
+	case Number(n: Int)
+	case Sum(e1: Expr, e2: Expr)
+	case Prod(e1: Expr, e2: Expr)
+
+def eval(e: Expr): Int = e match
+	case Expr.Number(n)    => n
+	case Expr.Sum(e1, e2)  => eval(e1) + eval(e2)
+  case Expr.Var(x)       => ...
+  case Expr.Prod(e1, e2) => ...
+
+def show(e: Expr): String = e match
+	case Expr.Var(x) => x
+	case Expr.Number(n) => n.toString
+	case Expr.Sum(a, b) => s”${show(a)} + ${show(a)}}”
+	case Expr.Prod(a, b) => s”${showP(a)} * ${showP(a)}”
+
+def showP(e: Expr): String = e match
+	case sum: Expr.Sum => s”(${show(sum)})”
+	case _ => show(e)
+
+// ==========================================================
+
+// direction enum from 3.3
+enum Direction(val dx: Int, val dy: Int):
+	case Up extends Direction( 0, 1)
+	case Right extends Direction( 1, 0)
+	case Down extends Direction( 0, -1)
+	case Left extends Direction(-1, 0)
+
+	def rightTurn = Direction.values((ordinal + 1) % 4)
+end Direction
